6b2b8627aae0a609a4bc27035e792731febeba75
SobarrSoFarr <dinnfavboyz@gmail.com>
2025-09-30 19:56:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const tombolSapa = document.getElementById('sapaButton);
-tombolSapa.addEventListener('click',function(){
-alert('Halo! Terima kasih telah berkunjung!');
+const tombolSapa = document.getElementById('SapaButton');
+tombolSapa.addEventListener('click', function() {
+  alert('Halo! Terima kasih telah berkunjung!');
+ // Tambahkan kelas animasi
+  tombolSapa.classList.add('klik-efek');
+  // Hapus kelas setelah 300ms supaya bisa dipakai lagi
+  setTimeout(() => {
+    tombolSapa.classList.remove('klik-efek');
+  }, 300);
 });
